0b4e6c25d8082a8a2299248db31c89b28988ebc7
Kumar Akshay <k.akshay9721@gmail.com>
2018-06-10 02:00:09 +0530

    Remove build

--- build/lib/verify_email/usage.py
+++
@@ -1,8 +0,0 @@
-from verify_email import validate_email
-from verify_email import get_mx_ip
-
-emails = [] # add emails
-
-for email in emails:
-    result = validate_email(email, debug=True)
-    print(result)
