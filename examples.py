2e79600c7172af674ec506b7e8923610b6d59995
Kumar Akshay <k.akshay9721@gmail.com>
2019-10-20 15:03:12 +0530

    Add Support for async and remove thread

--- examples.py
+++ examples.py
@@ -1,45 +1,32 @@
-import dns.resolver
 from verify_email import verify_email
-from verify_email import fast_verify_email
 import time
 
 
-resolver = dns.resolver.get_default_resolver()
-resolver.nameservers[0] = '8.8.8.8'
-
+emails = [
+    'k.akshay9721@gmail.com',
+    'some.email.address.that.does.not.exist@gmail.com',
+    'foo@bar.com',
+    'ex@example.com'
+]
 
 def time_it(func, *args, **kwargs):
     start = time.time()
     func(*args, **kwargs)
-    print("used:", time.time() - start, "seconds")
-
-emails = [
-    "k.akshay9721@gmail.com",
-    "some.email.address.that.does.not.exist@gmail.com",
-    "foo@bar.com",
-    "ex@example.com"
-]
+    print('used:', time.time() - start, 'seconds')
 
 
-def test_single(email):
-    print(email, "is", verify_email(email) and "valid" or "invalid")
+def single(email):
+    print(email, 'is', verify_email(email) and 'valid' or 'invalid')
 
 
-def test_multiple():
-    print("\t".join(emails))
-    print("\t".join(i and "valid" or "invalid" for i in verify_email(emails)))
+def multiple():
+    print('\t'.join(emails))
+    print('\t'.join(i and 'valid' or 'invalid' for i in verify_email(emails)))
 
 
-def test_threaded():
-    print("\t".join(emails))
-    print("\t".join(i and "valid" or "invalid" for i in fast_verify_email(emails)))
-
-
-print("### single")
+print('### single')
 for email in emails:
-    time_it(test_single, email)
-print("### multiple")
-time_it(test_multiple)
-print("### threaded")
-time_it(test_threaded)
-print("note that network condition may have impact on test results")
+    time_it(single, email)
+print('### multiple')
+time_it(multiple)
+print('note that network condition may have impact on test results')
--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="verify_email",
-    version="2.3.1",
-    author="Kumar Akshay",
-    author_email="k.akshay9721@gmail.com",
-    description="A small package for email verification",
+    name='verify_email',
+    version='2.4.1',
+    author='Kumar Akshay',
+    author_email='k.akshay9721@gmail.com',
+    description='A small package for email verification',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/kakshay21/verify_email",
+    long_description_content_type='text/markdown',
+    url='https://github.com/kakshay21/verify_email',
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 2.7",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.7+',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ),
     install_requires=[
         # -*- Extra requirements: -*-
-        'dnspython>=1.15.0',
+        'aiosmtpd',
+        'aiodns'
     ],
 )
