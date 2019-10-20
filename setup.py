0b90c3feb51fad27d3c4b228bf711ef34874ebf2
Kumar Akshay <k.akshay9721@gmail.com>
2019-10-20 15:20:58 +0530

    edit python version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     url='https://github.com/kakshay21/verify_email',
     packages=setuptools.find_packages(),
     classifiers=(
-        'Programming Language :: Python :: 3.7+',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ),
